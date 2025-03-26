f5cac06617a7119df5e368feb15d894a16ad6dc2
allkamau <allangithaiga5@gamil.com>
2025-03-26 17:47:42 +0300

    Add Tailwind CSS configuration for styling and theming

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#242938',
+        secondary: '#646CFF',
+        accent: '#535BF2',
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
