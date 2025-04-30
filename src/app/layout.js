9b108a40309584142d5a2453c11d4e1ada9455fb
Githaiga22 <allangithaiga5@gmail.com>
2025-05-01 00:47:27 +0300

    add: layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,9 @@
+import { neueMontrealRegular } from './fonts'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${neueMontrealRegular.variable}`}>
+      {/* ... rest of your layout */}
+    </html>
+  )
+} 
